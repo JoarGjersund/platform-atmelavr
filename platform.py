de2fd4680a9c183c09ceb3bb340bc297f5c1b579
Joar Andreas Gjersund <joargjersund@gmail.com>
2019-08-15 15:10:55 +0200

    copy from atmel sam
    debug tool

--- platform.py
+++ platform.py
@@ -50,3 +50,68 @@ class AtmelavrPlatform(PlatformBase):
             self.on_run_out(line)
         else:
             PlatformBase.on_run_err(self, line)
+            
+    def _add_default_debug_tools(self, board):
+        debug = board.manifest.get("debug", {})
+        upload_protocols = board.manifest.get("upload", {}).get(
+            "protocols", [])
+        if "tools" not in debug:
+            debug['tools'] = {}
+
+        # Atmel Ice / J-Link / BlackMagic Probe
+        tools = ("blackmagic", "jlink", "atmel-ice", "cmsis-dap", "stlink")
+        for link in tools:
+            if link not in upload_protocols or link in debug['tools']:
+                continue
+            if link == "blackmagic":
+                debug['tools']['blackmagic'] = {
+                    "hwids": [["0x1d50", "0x6018"]],
+                    "require_debug_port": True
+                }
+
+            elif link == "jlink":
+                assert debug.get("jlink_device"), (
+                    "Missed J-Link Device ID for %s" % board.id)
+                debug['tools'][link] = {
+                    "server": {
+                        "package": "tool-jlink",
+                        "arguments": [
+                            "-singlerun",
+                            "-if", "SWD",
+                            "-select", "USB",
+                            "-device", debug.get("jlink_device"),
+                            "-port", "2331"
+                        ],
+                        "executable": ("JLinkGDBServerCL.exe"
+                                       if system() == "Windows" else
+                                       "JLinkGDBServer")
+                    },
+                    "onboard": link in debug.get("onboard_tools", [])
+                }
+
+            else:
+                openocd_chipname = debug.get("openocd_chipname")
+                assert openocd_chipname
+                openocd_cmds = ["set CHIPNAME %s" % openocd_chipname]
+                if link == "stlink" and "at91sam3" in openocd_chipname:
+                    openocd_cmds.append("set CPUTAPID 0x2ba01477")
+                server_args = [
+                    "-s", "$PACKAGE_DIR/scripts",
+                    "-f", "interface/%s.cfg" % (
+                        "cmsis-dap" if link == "atmel-ice" else link),
+                    "-c", "; ".join(openocd_cmds),
+                    "-f", "target/%s.cfg" % debug.get("openocd_target")
+                ]
+                debug['tools'][link] = {
+                    "server": {
+                        "package": "tool-openocd",
+                        "executable": "bin/openocd",
+                        "arguments": server_args
+                    },
+                    "onboard": link in debug.get("onboard_tools", [])
+                }
+                if link == "stlink":
+                    debug['tools'][link]['load_cmd'] = "preload"
+
+        board.manifest['debug'] = debug
+        return board
